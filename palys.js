e058d4369847e28fe022037186576774d02cb2e7
Derek <dereksyme@gmail.com>
2019-04-07 17:11:47 -0400

    updated

--- palys.js
+++ palys.js
@@ -2,7 +2,7 @@ const go = async ()=>{
 	return 1;
 }
   
-  f().then((res)=>{
+  go().then((res)=>{
 	  console.log(res);
 	  console.log(typeof(res));
   });
--- play.js
+++ play.js
@@ -2,41 +2,65 @@ const myUtil = require('util');
 
 const claim = {name:'claim1'};
 
-const exportsModule = async()=>{
-	
-		if(1==1){
-			console.log('1 = 1');
+const rewards = [];
+
+const exportsModule = async(rewardLength)=>{
+
+		if(rewardLength === 0){
+			//random procedures
+			if(1==1){
+				console.log('1 = 1');
+			} else{
+				console.log('1 != 1');
+			}
+
+			setTimeout(()=>{
+				console.log('first timer');
+
+			},3000);
+
+			//Atlas Request
+			let func = ()=>{
+				return new Promise((resolve,reject)=>{
+					setTimeout(()=>{
+						console.log('timer done');
+						
+							resolve([{paymode:'Reward'}]);
+
+					},3000);
+				});
+			};
+			return await func();
+
 		} else{
-			console.log('1 != 1');
+			return 'paymodeSettingNotNecessary';
 		}
-
-		setTimeout(()=>{
-			console.log('first timer');
-
-		},3000);
-
-
-		let func = ()=>{
-			return new Promise((resolve,reject)=>{
-				setTimeout(()=>{
-					console.log('timer done');
-					
-						resolve([{paymode:'Reward'}]);
-
-				},3000);
-			});
-		};
-		return await func();
 };
 
 
-let getPaymode = exportsModule()
-	.then((t)=>{
-		console.log('Paymode: ' + t[0].paymode)
-	})
-	.catch((t)=>{
-		console.log('Failure: ' + t);
+let validateClaim = (theClaim)=>{
+	
+	return new Promise((resolve,reject)=>{
+		console.log(myUtil.inspect(theClaim));
+		claim.isValid = true
+		resolve(theClaim);
+	});
+}
+
+
+exportsModule(rewards.length)
+.then((res)=>{
+	console.log('result of exportsModule:' + myUtil.inspect(res));
+	claim.rewards = res;
+	return validateClaim(claim);
+})
+.then((validatedClaim) => {
+	console.log('validatedClaim:' + myUtil.inspect(validatedClaim));
+})
+.catch((t)=>{
+	console.log('Failure: ' + t);
 });
 
 
-console.log(getPaymode);
+
+//console.log(getPaymode);
